18956a554b22b841bd43ff135e58ebcb1ea24d75
v0agent <it+v0agent@vercel.com>
2026-06-04 02:29:44 +0000

    feat: update icon imports to FontAwesome icons in ICPSection component

--- components/icp-section.tsx
+++ components/icp-section.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Check, X } from 'react-icons/fa6'
+import { FaCheck, FaX } from 'react-icons/fa6'
 
 const yesItems = [
   "5+ years in a corporate or professional role (FAANG, finance, consulting, law)",
@@ -43,7 +43,7 @@ export function ICPSection() {
                 key={index}
                 className="text-sm text-cream pl-[26px] relative leading-[1.5] before:absolute before:left-0 before:text-green before:font-bold"
               >
-                <Check className="absolute left-0 top-0.5 text-green" size={16} />
+                <FaCheck className="absolute left-0 top-0.5 text-green" size={16} />
                 {item}
               </li>
             ))}
@@ -60,7 +60,7 @@ export function ICPSection() {
                 key={index}
                 className="text-sm text-cream pl-[26px] relative leading-[1.5] before:absolute before:left-0 before:text-red before:font-bold"
               >
-                <X className="absolute left-0 top-0.5 text-red" size={16} />
+                <FaX className="absolute left-0 top-0.5 text-red" size={16} />
                 {item}
               </li>
             ))}
